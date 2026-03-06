6ce4b816d1a0990685906b163f37a5c925f0317e
Chirs5 <2803570490@qq.com>
2026-03-06 10:39:08 +0800

    Update model configuration for Hf-Depth swin

--- configs/depthformer/Hf-Depth_swinl_22k_w7_kitti.py
+++ configs/depthformer/Hf-Depth_swinl_22k_w7_kitti.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/depthformer_swin.py', '../_base_/datasets/kitti.py',
+    '../_base_/models/Hf-Depth_swin.py', '../_base_/datasets/kitti.py',
     '../_base_/default_runtime.py'
 ]
 
